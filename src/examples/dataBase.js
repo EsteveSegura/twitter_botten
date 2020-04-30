afaee8ca735b670b181801cd3cc012109156a268
estevesegura <estevesegura.gir@gmail.com>
2020-04-30 03:33:53 +0200

    sentiment ES, database mongo, analisis sentiment working

--- src/examples/dataBase.js
+++ src/examples/dataBase.js
@@ -6,13 +6,21 @@
     const db = new dataBase('mongodb://localhost/twitter_botten')
     const bot = new twitterBot(process.env.CONSUMER_KEY,process.env.CONSUMER_SECRET,process.env.ACCESS_TOKEN, process.env.ACCESS_TOKEN_SECRET);
 
-    let tweetsSad = await bot.searchUsersByExactTextInTweet('dia de mierda', true, 200)
+    const sadSentences = ['dia de mierda', 'que mierda de todo', 'termine la serie', 'se termino todo']
 
-    for(let i = 0 ; i < tweetsSad.length; i++){
-        await db.addNewTweet(tweetsSad[i]);
-        let authorOfTweet = await bot.getUserByScreenName(tweetsSad[i].getUser());
-        await db.addNewUser(authorOfTweet);
-        console.log(`-Tweet Added to DB: ${tweetsSad[i].getText()}`)
-        console.log(`-User Added to DB: ${tweetsSad[i].getUser()}`)
+    for(let b = true; b == true; b = true){
+        sadSentences.forEach(async (sentence) => {
+            let tweetsSad = await bot.searchUsersByExactTextInTweet(sentence, true, 200)
+            
+            for(let i = 0 ; i < tweetsSad.length; i++){
+                
+                await db.addNewTweet(tweetsSad[i]);
+                let authorOfTweet = await bot.getUserByScreenName(tweetsSad[i].getUser());
+                await db.addNewUser(authorOfTweet);
+                console.log(`-Tweet Added to DB: ${tweetsSad[i].getText()}`)
+                console.log(`-User Added to DB: ${tweetsSad[i].getUser()}`)
+            }
+        });
+        await bot.sleep(300)
     }
 })();
